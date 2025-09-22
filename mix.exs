e772218dc1de4afa0659fa7a691878beb6c038bf
Siraj Samsudeen <mailsiraj@gmail.com>
2025-09-22 09:47:46 +0530

    gen auth

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule Calmdo.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:igniter, "~> 0.6", only: [:dev, :test]},
       {:phoenix, "~> 1.8.1"},
       {:phoenix_ecto, "~> 4.5"},
