e56baf6720bcfe423ae4392d4bbcb182601d7bca
Siraj Samsudeen <mailsiraj@gmail.com>
2025-10-01 16:15:40 +0530

    move setting endpoint for phoenix_test to test config

--- config/config.exs
+++ config/config.exs
@@ -73,8 +73,6 @@ config :logger, :default_formatter,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-config :phoenix_test, :endpoint, CalmdoWeb.Endpoint
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
