0137b4a9b1ec2518ad8fa43358a8ed62d686d88f
Zaseem-BIsquared <zaseem@bisquared.com>
2025-11-12 11:03:16 +0530

    REFACTOR: test modularization #19

--- test/calmdo_web/feature/user_record_log_from_projects_page_test.exs
+++ test/calmdo_web/feature/user_record_log_from_projects_page_test.exs
@@ -17,10 +17,15 @@ defmodule CalmdoWeb.UserRecordLogFromProjectsPageTest do
     conn
     |> visit(~p"/projects/#{project}")
     |> click_link("Log Time")
-    |> fill_in("Duration in hours", with: 1)
-    |> fill_in("Notes", with: "some notes")
-    |> click_button("Save Activity log")
+    |> record_new_log(hours: 1, notes: "some notes")
     |> assert_text("Activity log created successfully")
     |> assert_path(~p"/projects/#{project}")
+  end
+
+  defp record_new_log(conn, hours: hours, notes: notes) do
+    conn
+    |> fill_in("Duration in hours", with: hours)
+    |> fill_in("Notes", with: notes)
+    |> click_button("Save Activity log")
   end
 end
